304fe69960f184b627cdfec0b6fc4293bcf9c511
Remy Masson <muryoh@gmail.com>
2021-04-20 00:39:59 +0200

    First commit - let's get going

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const config = {
+  host: process.env.HOST ?? "https://design-systems.dev",
+  apiPath: process.env.API_PATH ?? "api/v1",
+};
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { getInput, setFailed } from "@actions/core";
+import { context } from "@actions/github";
+import got from "got";
+import { config } from "./config";
+
+const token = getInput("repo-token");
+const componentId = getInput("component-id");
+const apiToken = getInput("api-token");
+const version = getInput("version");
+
+got
+  .post(`${config.host}/${config.apiPath}/github-actions/release`, {
+    json: {
+      apiToken,
+      repoToken: `${context.payload.repository!.owner.login}:${token}`,
+      wcId: componentId,
+      sha1: context.payload.after,
+      version,
+    },
+  })
+  .then((response) => console.log(response.body))
+  .catch((error) => setFailed(error.response.body));
